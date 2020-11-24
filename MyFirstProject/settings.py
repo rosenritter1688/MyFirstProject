f056f91d5a206761cdacd14741db00f4578955e9
Liang Tzu Hsuan <silent.punk@gmail.com>
2020-11-24 18:03:59 +0800

    ./manage.py migrate

--- MyFirstProject/settings.py
+++ MyFirstProject/settings.py
@@ -103,9 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'zh-hant'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
