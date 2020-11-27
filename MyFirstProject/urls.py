20f72ea8795c5ed048c870705e22266202c3f365
Liang Tzu Hsuan =-Destop <a15714t@reitaku.jp>
2020-11-27 16:14:28 +0800

    Revert "def item in food/views.py are added. html code can put under def but its not a proper way to do so"
    This reverts commit c03b6e056fcc41696748e1faa1808f6bb35be25b.

--- MyFirstProject/urls.py
+++ MyFirstProject/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('food/',include('food.urls')), # IE path http://127.0.0.1:8000/food/   will look at under folder food and look for urls.py
 ]
