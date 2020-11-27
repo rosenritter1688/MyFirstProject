c03b6e056fcc41696748e1faa1808f6bb35be25b
Liang Tzu Hsuan =-Destop <a15714t@reitaku.jp>
2020-11-27 14:51:18 +0800

    def item in food/views.py are added. html code can put under def but its not a proper way to do so

--- food/views.py
+++ food/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def helloworld(request):#request from the user
     return HttpResponse('Hello Wold')
+
+def item(request):
+    return HttpResponse('<h1>This is an item view</h1>')
+                        #? html code can put in there but its not a proper way to do so
